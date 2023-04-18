9251e6a6e93f3fc60e452eae5ea06ba816a453d5
GiapKhac <114390983+giap19102004@users.noreply.github.com>
2023-04-19 00:55:23 +0700

    Add files via upload

---
+++ Point/ExercisePoint2DAndPoint3D.java
@@ -0,0 +1,11 @@
+package Point;
+
+public class ExercisePoint2DAndPoint3D {
+    public static void main(String[] args) {
+        Point2D first2DPoint=new Point2D(3, 7);
+        Point3D first3DPoint=new Point3D(2, 6,3);
+
+        System.out.println(first2DPoint.toString());
+        System.out.println(first3DPoint.toString());
+    }
+}
